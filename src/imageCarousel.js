26dd3e5dd9c93268d5edd168289838c4476b4ea0
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-04-30 11:18:27 +0100

    fixed image carousel

--- src/imageCarousel.js
+++ src/imageCarousel.js
@@ -7,11 +7,43 @@ import streetImage3 from "./images/toxic-smoker.jpg";
 const imageCarousel = (function () {
   const imageArray = [streetImage1, streetImage2, streetImage3];
 
+  let slideIndex = 1;
+
+  const _nextPrevSlide = (n) => {
+    showSlides((slideIndex += n));
+  };
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("image-carousel");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.toggle("active");
+  }
+
   const drawImageCarouselPage = () => {
     const contentPage = domController.clearMainContentContainer();
     const imageCarouselContainerDiv = document.createElement("div");
     const nextBtn = document.createElement("a");
     const prevBtn = document.createElement("a");
+    const dotDiv = document.createElement("div");
+    dotDiv.setAttribute("text-align", "center");
 
     nextBtn.innerHTML = "&#10095;";
     nextBtn.classList.add("next");
@@ -25,7 +57,7 @@ const imageCarousel = (function () {
       _nextPrevSlide(-1);
     });
 
-    imageArray.forEach((image) => {
+    imageArray.forEach((image, index) => {
       const imageCarouselDiv = document.createElement("div");
       const imageBeingAdded = document.createElement("img");
 
@@ -35,55 +67,20 @@ const imageCarousel = (function () {
       imageCarouselDiv.classList.add("image-carousel");
       imageCarouselDiv.appendChild(imageBeingAdded);
       imageCarouselContainerDiv.appendChild(imageCarouselDiv);
+
+      const dotSpan = document.createElement("span");
+      dotSpan.classList.add("dot");
+      dotSpan.addEventListener("click", () => {
+        currentSlide(index);
+      });
+      dotDiv.appendChild(dotSpan);
     });
     imageCarouselContainerDiv.appendChild(nextBtn);
     imageCarouselContainerDiv.appendChild(prevBtn);
     imageCarouselContainerDiv.classList.add("slideshow-container");
     contentPage.appendChild(imageCarouselContainerDiv);
-
-    let slideIndex = 1;
+    contentPage.appendChild(dotDiv);
     showSlides();
-
-    const dotDiv = document.createElement("div");
-    dotDiv.setAttribute("text-align", "center");
-
-    for (let i = 0; i < imageArray.length; i++) {
-      const dotSpan = document.createElement("span");
-      dotSpan.className.add("dot");
-      dotSpan.addEventListener("click", () => {
-        currentSlide(i);
-      });
-    }
-
-    const _nextPrevSlide = (n) => {
-      showSlides((slideIndex += n));
-    };
-
-    function currentSlide(n) {
-      showSlides((slideIndex = n));
-    }
-
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("image-carousel");
-      let dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {
-        slideIndex = 1;
-      }
-      if (n < 1) {
-        slideIndex = slides.length;
-      }
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex - 1].style.display = "block";
-      dots[slideIndex - 1].className += "active";
-
-      setTimeout(showSlides, 2000);
-    }
   };
 
   return { drawImageCarouselPage };
